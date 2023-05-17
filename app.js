cd1bf7b2f0ba6bd1db3c7c439bd70cdbb9edd1c7
lukaszgromadzki <28166634+lukaszgromadzki@users.noreply.github.com>
2023-05-17 13:20:47 +0200

    playerFactory added

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello")
+console.log("hello");
+
+const gameBoard = (() => {
+  console.log("gameboard");
+  let gameBoardArray = [];
+})();
+
+
+const playerFactory = (nick, marker) => {
+    const whatMarker = () => console.log("marker", marker);
+    return {nick, marker, whatMarker}
+}
+
+const playerO = playerFactory('playerO', 'O');
+const playerX = playerFactory('playerX', 'X');
+
+playerO.whatMarker();
+playerX.whatMarker();
