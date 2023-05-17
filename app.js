3edef4b753991a6bc58b9161715fceed0625b2b0
lukaszgromadzki <28166634+lukaszgromadzki@users.noreply.github.com>
2023-05-17 14:18:36 +0200

    show array items in html

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-console.log("hello");
+console.log("hello JS");
 
 const gameBoard = (() => {
   console.log("gameboard");
-  let gameBoardArray = [];
+  let gameBoardArray = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+  const showBoard = () => {
+    const gameHtml = document.querySelector("#gameBoard");
+
+    gameBoardArray.forEach((item, index) => {
+        const gameCell = document.createElement("div");
+        gameCell.classList.add("gameCell")
+        gameCell.textContent = item;
+        gameHtml.appendChild(gameCell)
+    })
+}
+  
+  return {
+    showBoard
+  }
 })();
+
+gameBoard.showBoard();
 
 
 const playerFactory = (nick, marker) => {
     const whatMarker = () => console.log("marker", marker);
     return {nick, marker, whatMarker}
 }
+
+
+const gameController = (() => {
+    console.log("gameController")
+})
+
+
 
 const playerO = playerFactory('playerO', 'O');
 const playerX = playerFactory('playerX', 'X');
