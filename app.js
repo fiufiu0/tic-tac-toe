ce0ce3ec0baaf9aea9c8e0e1bcc6e04751cc742d
lukaszgromadzki <28166634+lukaszgromadzki@users.noreply.github.com>
2023-05-17 18:51:06 +0200

    event listener for every cell

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
-console.log("hello JS");
+// Player factory
+const playerFactory = (nick, marker) => {
+  const whatMarker = () => console.log("marker", marker);
+  return { nick, marker, whatMarker };
+};
 
+// Game board module IIFE
 const gameBoard = (() => {
-  console.log("gameboard");
+  console.log("gameboard run");
+  const gameHtml = document.querySelector("#gameBoard");
   let gameBoardArray = [];
 
-  for(let i = 0; i < 9; i++){
-    gameBoardArray.push('');
+  for (let i = 0; i < 9; i++) {
+    gameBoardArray.push("");
   }
 
-  const showBoard = () => {
-    const gameHtml = document.querySelector("#gameBoard");
+  gameBoardArray.forEach((item, index) => {
+    const gameCell = document.createElement("div");
+    gameCell.classList.add("gameCell");
+    gameCell.textContent = item;
+    gameCell.dataset.index = index;
+    gameHtml.appendChild(gameCell);
+  });
 
-    gameBoardArray.forEach((item, index) => {
-        const gameCell = document.createElement("div");
-        gameCell.classList.add("gameCell")
-        gameCell.textContent = item;
-        gameHtml.appendChild(gameCell)
-    })
-}
-  
+  gameHtml.addEventListener("click", (item) => {
+    let indexCell = item.target.dataset.index;
+    console.log(item);
+    gameBoardArray[indexCell] = gameController.defaultPlayer.marker;
+    item.target.textContent = gameController.defaultPlayer.marker;
+    console.log(gameBoardArray)
+
+  });
+
   return {
-    showBoard
-  }
+    gameBoardArray,
+  };
 })();
 
-gameBoard.showBoard();
-
-
-const playerFactory = (nick, marker) => {
-    const whatMarker = () => console.log("marker", marker);
-    return {nick, marker, whatMarker}
-}
-
-
+// Game controller module IIFE
 const gameController = (() => {
-    console.log("gameController")
-})
+  console.log("gameController run");
 
+  const playerO = playerFactory("playerO", "O");
+  const playerX = playerFactory("playerX", "X");
 
+  const defaultPlayer = playerO;
 
-const playerO = playerFactory('playerO', 'O');
-const playerX = playerFactory('playerX', 'X');
-
-playerO.whatMarker();
-playerX.whatMarker();
+  return {
+    defaultPlayer
+  }
+})();
